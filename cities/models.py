3b912b8ac6df26ed97c3c2ca12e5ea6875e733b2
philipotieno <tmitchellb007@gmail.com>
2020-03-03 09:53:07 +0300

    Add cities app

---
+++ cities/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.gis.db import models as geomodels
+
+class City(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    geometry = geomodels.PointField()
+
+    class Meta:
+        # oder fo drop-down list items
+        ordering = ('name',)
+
+        # plural form in admin view
+        verbose_name_plural = 'cities'
