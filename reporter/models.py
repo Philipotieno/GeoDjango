278afb2992b101193e133190008dab3a4142f207
philipotieno <tmitchellb007@gmail.com>
2020-03-04 09:55:01 +0300

    adds models

--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Incidence(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Child's First Name and Last Name")
+    location = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        # verbose_name = 'Incidence'
+        verbose_name_plural = "Incidences"
