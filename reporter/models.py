87d9f24bf1ecbd20d1bd6aee244e9f587134be72
philipotieno <tmitchellb007@gmail.com>
2020-03-05 19:36:24 +0300

    adjusts models

--- reporter/models.py
+++ reporter/models.py
@@ -1,13 +1,17 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.gis.db import models
+from django.db.models import Manager as GeoManager
+
 
 # Create your models here.
 class Incidence(models.Model):
-    name = models.CharField(max_length=20, verbose_name="Child's First Name and Last Name")
-    location = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    location = models.PointField(srid=4326)
+    # objects = models.GeoManager()
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        # verbose_name = 'Incidence'
         verbose_name_plural = "Incidences"
